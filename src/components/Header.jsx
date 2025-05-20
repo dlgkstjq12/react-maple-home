a5bbf4496282c9b8ccb91ca7ed5443c6b9b8c738
이한섭 <dlgkstjq623@naver.com>
2025-05-20 20:38:26 +0900

    [이한섭]모바일 메뉴바 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -7,6 +7,7 @@ const Header = () => {
 
   // 모바일에서 메뉴아이콘 클릭시 실행
   const toggleVisibility = () => {
+
     const div = document.getElementById("toDiv");
     const costumHeader = document.getElementsByClassName("costum_header");
     const MAIN_MEUM_NUM = 0;
@@ -29,76 +30,66 @@ const Header = () => {
   return (
     <div className="header_section">
       <div className="costum_header">
-        <img
-          className="menu-logo-div"
-          onClick={() => navigate("/")}
-          src="images/MapleFighter.jpg"
-          alt="이미지"
-        />
+        <img className="menu-logo-div" onClick={() => navigate("/")} src="images/MapleFighter.jpg" alt="이미지"/>
         <h4 className="main-menu-name" onClick={() => navigate("/")}>
           Maple-Fighter
         </h4>
-
         <div className="small_menu">
           <ul>
             <li onClick={() => navigate("/")}>
-              <a className={getMenuClass("/")}>메인화면</a>
+              <Link className={getMenuClass("/")}>
+                메인화면
+              </Link>
             </li>
             <li onClick={() => navigate("/CharPotialComp")}>
-              <a className={getMenuClass("/CharPotialComp")}>
+              <Link className={getMenuClass("/CharPotialComp")}>
                 잠재능력 시행횟수 비교 분석 (개발중)
-              </a>
+              </Link>
             </li>
             <li onClick={() => navigate("/Contact")}>
-              <a className={getMenuClass("/Contact")}>Contact Me</a>
+              <Link className={getMenuClass("/Contact")}>
+                Contact Me
+              </Link>
             </li>
             <li onClick={() => navigate("/PrivacyPolicy")}>
-              <a className={getMenuClass("/PrivacyPolicy")}>
+              <Link className={getMenuClass("/PrivacyPolicy")}>
                 개인정보 처리방침
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
 
         <div className="toggle-div">
-          <img
-            className="toggle-icon"
-            src="images/toggle-icon.png"
-            onClick={toggleVisibility}
-          />
+          <img className="toggle-icon" src="images/toggle-icon.png" onClick={toggleVisibility}/>
         </div>
 
         <nav id="toDiv" className="Header_mobile-navigation-menus">
           <ul>
-            <li
-              onClick={() => navigate("/")}
-              className={`mobile-navigation ${getMenuClass("/")}`}
-            >
-              메인화면
+            <li onClick={() => navigate("/")}>
+              <Link className={`mobile-navigation ${getMenuClass("/")}`} onClick={toggleVisibility}>
+                메인화면
+              </Link>
             </li>
           </ul>
           <ul>
-            <li
-              onClick={() => navigate("/CharPotialComp")}
-              className={`mobile-navigation ${getMenuClass("/CharPotialComp")}`}
-            >
-              잠재능력 시행횟수 비교 분석 (개발중)
+            <li onClick={() => navigate("/CharPotialComp")}>
+              <Link className={`mobile-navigation ${getMenuClass("/CharPotialComp")}`} onClick={toggleVisibility}>
+                잠재능력 시행횟수 비교 분석 (개발중)
+              </Link>
             </li>
           </ul>
           <ul>
-            <li
-              onClick={() => navigate("/Contact")}
-              className={`mobile-navigation ${getMenuClass("/Contact")}`}
-            >
-              Contact Me
+            <li onClick={() => navigate("/Contact")}>
+              <Link className={`mobile-navigation ${getMenuClass("/Contact")}`} onClick={toggleVisibility}>
+                Contact Me
+              </Link>
             </li>
           </ul>
           <ul>
-            <li
-              onClick={() => navigate("/PrivacyPolicy")}
-              className={`mobile-navigation ${getMenuClass("/PrivacyPolicy")}`}
-            >
-              개인정보 처리방침
+            <li onClick={() => navigate("/PrivacyPolicy")}>
+              <Link className={`mobile-navigation ${getMenuClass("/PrivacyPolicy")}`} onClick={toggleVisibility}>
+                개인정보 처리방침
+              </Link>
             </li>
           </ul>
         </nav>
